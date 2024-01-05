7d31222e95f401ea2462e5560fd486fc7cf283ca
David <dvdcharles222@gmail.com>
2024-01-04 23:01:25 -0300

    Estudando repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+//TESTE LOGICO NO INÍCIO
+var c = 1
+
+while (c <= 6){
+    console.log('tudo bem?')
+    c++
+}
+
+
+//TESTE LOGICO NO FINAL
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=6)
+
+
+
+/*
+console.log('Tudo Bem?)
+console.log('Tudo Bem?)
+console.log('Tudo Bem?)
+console.log('Tudo Bem?)
+console.log('Tudo Bem?)
+console.log('Tudo Bem?)
+ */
